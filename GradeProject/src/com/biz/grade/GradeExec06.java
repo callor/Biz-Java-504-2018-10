f050ea80876834151bc34e2d7fc40f991303e772
callor <callor@callor.com>
2018-11-30 12:53:11 +0900

    181130

--- GradeProject/src/com/biz/grade/GradeExec06.java
+++ GradeProject/src/com/biz/grade/GradeExec06.java
@@ -1,5 +1,9 @@
 package com.biz.grade;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +11,57 @@ import com.biz.grade.vo.StudentVO;
 
 public class GradeExec06 {
 
+	/*
+	 * 영어이름들. txt 파일을 읽어서
+	 * 각 라인을 : 기준으로 자른 후
+	 * sVO 리스트에 추가하시오
+	 * 1:Korea:대한민국
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String nameFile
 		= "src/com/biz/grade/영어이름들.txt";
 		
 		List<StudentVO> sVO = new ArrayList();
+		
+		FileReader fr;
+		BufferedReader buffer;
+		
+		try {
+			fr = new FileReader(nameFile);
+			buffer = new BufferedReader(fr);
+			while(true) {
+				String strLine = buffer.readLine();
+				if(strLine == null) break;
+				
+				String[] strNames = strLine.split(":");
+				if(strNames.length > 2) {
+					
+					StudentVO vo = new StudentVO();
+					vo.setStrNum(strNames[0]);
+					vo.setStrEngName(strNames[1]);
+					vo.setStrKorName(strNames[2]);
+					sVO.add(vo);
+
+				}
+			}
+			buffer.close();
+			fr.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(StudentVO vo : sVO) {
+			System.out.println(vo);
+		}
+		
+		
+		
 		
 
 	}
