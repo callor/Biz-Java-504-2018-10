393f57387b1345695a4dace9a199df5971ed9373
callor <callor@callor.com>
2018-12-06 15:51:15 +0900

    181206

--- FileInout01/src/com/biz/files/service/UserSe01.java
+++ FileInout01/src/com/biz/files/service/UserSe01.java
@@ -9,6 +9,7 @@ import com.biz.files.vo.UserVO;
 
 public class UserSe01 {
 
+
 	List<UserVO> userList;
 	UserVO vo ;
 	
@@ -18,6 +19,7 @@ public class UserSe01 {
 	}
 	
 	public void makeId() {
+		List<String> strList = new ArrayList();
 		for(int i = 0 ; i < 10 ; i ++) {
 
 			// 10개의 리스트에 담긴 vo는 항상
@@ -25,16 +27,23 @@ public class UserSe01 {
 			UserVO v = new UserVO();
 			
 			int num = (int)(Math.random() * (200-100+1)) + 100;
+			strList.add("" + num);
+
+			/*
 			vo.setStrId("" + num);
 			v.setStrId(""+num);
-//			userList.add(vo);
+			userList.add(vo);
 			userList.add(v);
+			*/
 			
 		}
-//		Set<UserVO> uSet = new TreeSet(userList);
-//		userList = new ArrayList(uSet);
 		
-		
+		strList = new ArrayList(new TreeSet(strList));
+		for(String s : strList) {
+			UserVO vo = new UserVO();
+			vo.setStrId(s);
+			userList.add(vo);
+		}
 	}
 	
 	public void view() {
