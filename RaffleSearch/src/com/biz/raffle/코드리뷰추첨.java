89d45383758d913745795cbe4756d84978e2301e
callor <callor@callor.com>
2019-01-24 12:35:50 +0900

    20190123

--- RaffleSearch/src/com/biz/raffle/코드리뷰추첨.java
+++ RaffleSearch/src/com/biz/raffle/코드리뷰추첨.java
@@ -10,19 +10,19 @@ public class 코드리뷰추첨 {
 		// TODO Auto-generated method stub
 		List<String> nameList = new ArrayList();
 
-		// nameList.add("고은향");
-		// nameList.add("고재범");
-		// nameList.add("대재민");
-		nameList.add("문지현");
-		// nameList.add("안정하");
-		// nameList.add("이상일");
-		// nameList.add("이종환");
-		// nameList.add("이주현");
-		// nameList.add("이주훈");
-		// nameList.add("이준수");
-		// nameList.add("임동인"); // 금요일 발표
-		// nameList.add("정효순");
-		// nameList.add("조동혁");
+//		 nameList.add("고은향");
+		 nameList.add("고재범");
+//		 nameList.add("대재민");
+//		nameList.add("문지현");
+//		 nameList.add("안정하");
+//		 nameList.add("이상일");
+		 nameList.add("이종환");
+//		 nameList.add("이주현");
+//		 nameList.add("이주훈");
+//		 nameList.add("이준수");
+//		 nameList.add("임동인"); // 금요일 발표
+//		 nameList.add("정효순");
+//		 nameList.add("조동혁");
 
 		for (int i = 0; i < nameList.size(); i++) {
 			Collections.shuffle(nameList);
