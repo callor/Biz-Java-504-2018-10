d34ff264df30e085028310ac9c14a795969fb06a
callor <callor@callor.com>
2019-02-19 12:35:57 +0900

    2019-02-19

--- RaffleSearch/src/com/biz/raffle/코드리뷰추첨.java
+++ RaffleSearch/src/com/biz/raffle/코드리뷰추첨.java
@@ -11,16 +11,16 @@ public class 코드리뷰추첨 {
 		List<String> nameList = new ArrayList();
 
 //		 nameList.add("고은향");
-		 nameList.add("고재범");
+//		 nameList.add("고재범");
 //		 nameList.add("대재민");
-//		nameList.add("문지현");
+//		 nameList.add("문지현");
 //		 nameList.add("안정하");
 //		 nameList.add("이상일");
-		 nameList.add("이종환");
+//		 nameList.add("이종환");
 //		 nameList.add("이주현");
 //		 nameList.add("이주훈");
-//		 nameList.add("이준수");
-//		 nameList.add("임동인"); // 금요일 발표
+		 nameList.add("이준수");
+//		 nameList.add("임동인"); 
 //		 nameList.add("정효순");
 //		 nameList.add("조동혁");
 
